e5d21a34bb2a4332485093b5ddf3fab82e316030
Yukio Nozawa <personal@nyanchangames.com>
2025-03-02 17:05:30 +0900

    format

--- viewKit/__init__.py
+++ viewKit/__init__.py
@@ -5,8 +5,8 @@ from .viewCreator import *
 
 
 def run(first_window):
-    #dllを相対パスで指定した時のため、カレントディレクトリを変更
-    if sys.version_info.major>=3 and sys.version_info.minor>=8:
+    # dllを相対パスで指定した時のため、カレントディレクトリを変更
+    if sys.version_info.major >= 3 and sys.version_info.minor >= 8:
         os.add_dll_directory(os.getcwd())
         sys.path.append(os.getcwd())
     app = App(first_window)
