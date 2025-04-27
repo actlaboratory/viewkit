a4e04a549d7474b6055b6da2a01017c7ddb089bb
yamahubuki <itiro.ishino@gmail.com>
2025-04-27 18:41:00 +0900

    起動時のパス追加処理を追加

--- viewkit/app.py
+++ viewkit/app.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import wx
 from viewkit.context.app import ApplicationContext
 
@@ -11,6 +13,7 @@ class App(wx.App):
 
     def run(self):
         """ウインドウを表示して、アプリケーションを開始。アプリケーションが終了するまで制御を返さない"""
+        self._addPath()
         wnd = self._initial_window(self.ctx)
         wnd._register_features(wnd.define_features())
         wnd._assign_refs()
@@ -19,3 +22,18 @@ class App(wx.App):
         wnd._apply_accelerator_table()
         wnd.Show()
         self.MainLoop()
+
+    def _addPath(self):
+		"""sys.pathと、3.8以降の場合のdll読み込み対象パスにアプリケーション直下を追加"""
+        if sys.version_info.major>=3 and sys.version_info.minor>=8:
+            os.add_dll_directory(os.path.dirname(self.getAppPath()))
+        sys.path.append(os.path.dirname(self.getAppPath()))
+
+    def getAppPath(self):
+        """アプリの絶対パスを返す"""
+        if hasattr(sys,"frozen"):
+            # exeファイルで実行されている
+            return sys.executable
+        else:
+            # pyファイルで実行されている
+            return os.path.abspath(sys.argv[0])
