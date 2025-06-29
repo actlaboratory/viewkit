f46a9ffbebb346b0de955f724fc47b2c8c7fb18c
yamahubuki <itiro.ishino@gmail.com>
2025-06-29 15:57:02 +0900

    重複コードの削除

--- viewkit/__init__.py
+++ viewkit/__init__.py
@@ -9,9 +9,5 @@ from .menu import TopMenuDefinition, MenuItemDefinition, MenuDefinition, separat
 
 
 def run(ctx, first_window):
-    # dllを相対パスで指定した時のため、カレントディレクトリを変更
-    if sys.version_info.major >= 3 and sys.version_info.minor >= 8:
-        os.add_dll_directory(os.getcwd())
-        sys.path.append(os.getcwd())
     app = App(ctx, first_window)
     app.run()
