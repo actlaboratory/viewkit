a335576e9a5afde95dc8fa897095e4b5b5d0f00c
yamahubuki <itiro.ishino@gmail.com>
2025-08-16 20:07:09 +0900

    add dialog

---
+++ viewkit/dialog.py
@@ -0,0 +1,31 @@
+﻿import wx
+import ctypes
+import sys
+
+def dialog(parent, title, message):
+	dialog = wx.MessageDialog(parent,message,title,wx.OK)
+	dialog.ShowModal()
+	dialog.Destroy()
+
+def yesNoDialog(parent, title, message):
+	dialog = wx.MessageDialog(parent,message,title,wx.YES_NO)
+	result = dialog.ShowModal()
+	dialog.Destroy()
+	return result
+
+def errorDialog(parent, message):
+	dialog = wx.MessageDialog(parent,message,"error",wx.OK|wx.ICON_ERROR)
+	dialog.ShowModal()
+	dialog.Destroy()
+
+def debug(message):
+	if hasattr(sys, "frozen") == False:
+		if type(message) != str:
+			import pprint
+			message = pprint.pformat(message)
+		dialog = wx.MessageDialog(None,message,"debug",wx.OK)
+		dialog.ShowModal()
+		dialog.Destroy()
+
+def win(title, message):
+	ctypes.windll.user32.MessageBoxW(0,message,title,0x00000040)
