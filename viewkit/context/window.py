ff71db5fb58096c1d5ea8a6d637aaa816b483414
Yukio Nozawa <personal@nyanchangames.com>
2025-09-07 16:37:45 +0900

    Add logs

--- viewkit/context/window.py
+++ viewkit/context/window.py
@@ -1,3 +1,4 @@
+import logging
 import wx
 from viewkit.menu import Menu
 from viewkit.ref import RefStore
@@ -9,9 +10,13 @@ class WindowContext:
         self.menu = Menu()
         self.ref_store = RefStore()
         self.feature_store = FeatureStore()
+        self.logger = logging.getLogger(__name__)
+        self.logger.debug("WindowContext initialized")
+
 
     def generateAcceleratorTable(self):
         """すべての feature の wx.AcceleratorTable を作成する"""
+        self.logger.debug("Generating accelerator table")
         entries = []
         for feature in self.feature_store.all().values():
             for shortcut_key in feature.shortcut_keys:
