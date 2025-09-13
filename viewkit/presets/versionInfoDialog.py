1bed542408b17ffc11b894cd79579f785de73951
yamahubuki <itiro.ishino@gmail.com>
2025-09-13 17:28:11 +0900

    過去の修正漏れ

--- viewkit/presets/versionInfoDialog.py
+++ viewkit/presets/versionInfoDialog.py
@@ -5,7 +5,7 @@ from viewkit.version import getVersion
 
 
 class VersionInfoDialog(SubWindow):
-    def __init__(self, parent, ctx, title):
+    def __init__(self, parent, ctx, title, parameters):
         SubWindow.__init__(self, parent, ctx, title)
         textList = []
         textList.append("%s (%s)" % (self.app_ctx.application_name, self.app_ctx.short_name))
@@ -18,8 +18,8 @@ class VersionInfoDialog(SubWindow):
         textList.append(_("ライセンス/著作権情報については、同梱の license.txt を参照してください。"))
         textList.append("")
 
-        self.info, dummy = self.creator.inputbox("", defaultValue="\r\n".join(textList), style=wx.TE_MULTILINE |
-                                                 wx.TE_READONLY | wx.TE_NO_VSCROLL | wx.BORDER_RAISED, sizerFlag=wx.EXPAND, x=750, textLayout=None)
+        self.info, dummy = self.creator.inputbox("", default_value="\r\n".join(textList), style=wx.TE_MULTILINE |
+                                                 wx.TE_READONLY | wx.TE_NO_VSCROLL | wx.BORDER_RAISED, sizer_flag=wx.EXPAND, x=750, text_layout=None)
         f = self.info.GetFont()
         f.SetPointSize((int)(f.GetPointSize() * (2 / 3)))
         self.info.SetFont(f)
